15770b9c8d92404108ebe70cdf0b6f2d5689a765
David Adam <zanchey@ucc.gu.uwa.edu.au>
2018-12-28 22:07:47 +0800

    fish 3.0.0

--- Formula/fish.rb
+++ Formula/fish.rb
@@ -1,47 +1,28 @@
 class Fish < Formula
   desc "User-friendly command-line shell for UNIX-like operating systems"
   homepage "https://fishshell.com"
-  url "https://github.com/fish-shell/fish-shell/releases/download/2.7.1/fish-2.7.1.tar.gz"
-  sha256 "e42bb19c7586356905a58578190be792df960fa81de35effb1ca5a5a981f0c5a"
-
-  devel do
-    url "https://github.com/fish-shell/fish-shell/releases/download/3.0b1/fish-3.0b1.tar.gz"
-    sha256 "11c464ac9f2b838b0a9e3fb8e91f7727649d0d13122b5bcb7bc4e5199bc5e8a6"
-
-    depends_on "cmake" => :build
-    depends_on "doxygen" => :build
-  end
+  url "https://github.com/fish-shell/fish-shell/releases/download/3.0.0/fish-3.0.0.tar.gz"
+  sha256 "ea9dd3614bb0346829ce7319437c6a93e3e1dfde3b7f6a469b543b0d2c68f2cf"
 
   head do
     url "https://github.com/fish-shell/fish-shell.git", :shallow => false
 
-    depends_on "cmake" => :build
     depends_on "doxygen" => :build
   end
 
+  depends_on "cmake" => :build
   depends_on "pcre2"
 
   def install
-    if build.head? || build.devel?
-      args = %W[
-        -Dextra_functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d
-        -Dextra_completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d
-        -Dextra_confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d
-        -DSED=/usr/bin/sed
-      ]
-      system "cmake", ".", *std_cmake_args, *args
-    else
-      # In Homebrew's 'superenv' sed's path will be incompatible, so
-      # the correct path is passed into configure here.
-      args = %W[
-        --prefix=#{prefix}
-        --with-extra-functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d
-        --with-extra-completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d
-        --with-extra-confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d
-        SED=/usr/bin/sed
-      ]
-      system "./configure", *args
-    end
+    # In Homebrew's 'superenv' sed's path will be incompatible, so
+    # the correct path is passed into configure here.
+    args = %W[
+      -Dextra_functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d
+      -Dextra_completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d
+      -Dextra_confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d
+      -DSED=/usr/bin/sed
+    ]
+    system "cmake", ".", *std_cmake_args, *args
     system "make", "install"
   end
 
